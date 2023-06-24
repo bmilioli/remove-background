e6e2c940f26e0654ac2aa7ebd3b8551b119c42d0
Cascao <matheusfranco23@hotmail.com>
2023-06-24 20:47:26 -0300

    video to base64

---
+++ src/use_cases/remove_video_background/index.py
@@ -0,0 +1,26 @@
+import imageio
+import base64
+import os
+
+from config import settings
+
+class RemoveVideobackground:
+    def __init__(self,
+                 video_path
+    ):
+        self.video_path = video_path
+
+    def execute():
+        pass
+
+    def _extract_frames_from_video(self, video_path):
+        pass
+
+    def _remove_image_background(self, base_64_image):
+        pass
+
+    def _transform_image_to_base_64(self, image_path):
+        pass
+
+    def _concatenate_frames_to_video(self, frames):
+        pass
